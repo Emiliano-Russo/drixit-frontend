e6b20aec080d99cfd8ec0d3f43a70ac633413e47
Emiliano <russo.emiliano@gmail.com>
2022-08-19 14:04:51 -0300

    password implemented

--- drixit/src/App.tsx
+++ drixit/src/App.tsx
@@ -1,20 +1,26 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import "./App.css";
-import { Input } from "antd";
+import { Button, Input, Spin } from "antd";
 import "antd/dist/antd.css";
 
-function isEmailValid(email: string): boolean {
-  return true;
-}
-
 function App() {
-  const [email, setEmail] = useState("");
-  const [isTyping, setIsTyping] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [emailValid, setEmailValid] = useState<boolean>(false);
+  const [touchedInput, setTouchedInput] = useState<boolean>(false);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const [nextTouched, setNextTouched] = useState<boolean>(false);
+
+  function isEmailValid(email: string): boolean {
+    return email.includes("@") && !email.includes(" ") && email.includes(".");
+  }
 
   useEffect(() => {
     const timeoutID = setTimeout(() => {
-      console.log(email);
       setIsTyping(false);
+      const result = isEmailValid(email);
+      setEmailValid(result);
     }, 500);
     return () => {
       clearTimeout(timeoutID);
@@ -24,16 +30,66 @@ function App() {
   return (
     <div className="App">
       <h1>Login</h1>
-      <h1>is typing: {isTyping ? "yes" : "no"}</h1>
       <Input
-        placeholder="email"
+        disabled={nextTouched}
+        placeholder="Email"
         style={{ width: 300 }}
         value={email}
         onChange={(e) => {
+          if (e.target.value.length === 0) setTouchedInput(false);
+          else setTouchedInput(true);
           setIsTyping(true);
           setEmail(e.target.value);
         }}
+        suffix={
+          nextTouched ? (
+            <Button
+              type="text"
+              style={{ color: "green" }}
+              onClick={() => {
+                setNextTouched(false);
+              }}
+            >
+              Change
+            </Button>
+          ) : touchedInput ? (
+            isTyping ? (
+              <Spin size="small" />
+            ) : emailValid ? (
+              <CheckOutlined style={{ color: "green" }} />
+            ) : (
+              <CloseOutlined style={{ color: "red" }} />
+            )
+          ) : (
+            <></>
+          )
+        }
       ></Input>
+      <br></br>
+
+      <Input
+        hidden={!nextTouched}
+        placeholder="Password"
+        style={{ marginTop: 10 }}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      ></Input>
+
+      <Button
+        hidden={!emailValid || nextTouched}
+        style={{ marginTop: 10 }}
+        onClick={() => setNextTouched(true)}
+      >
+        Next
+      </Button>
+      <Button
+        hidden={!nextTouched}
+        disabled={password.length == 0}
+        style={{ marginTop: 10 }}
+      >
+        Login
+      </Button>
     </div>
   );
 }
