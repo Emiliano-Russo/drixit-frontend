d46e24bdc0559e395685b46cee0aeedb26f0e128
Emiliano <russo.emiliano@gmail.com>
2022-08-19 10:44:30 -0300

    React Init

---
+++ drixit/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <input type={"text"}></input>
+    </div>
+  );
+}
+
+export default App;
