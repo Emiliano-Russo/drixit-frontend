2a85b3cfedef3811c8049c0f107aed4e0b277a2b
Emiliano <russo.emiliano@gmail.com>
2022-08-19 12:06:50 -0300

    debounce basis

--- drixit/src/App.tsx
+++ drixit/src/App.tsx
@@ -1,12 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
+import { Input } from "antd";
+import "antd/dist/antd.css";
+
+function isEmailValid(email: string): boolean {
+  return true;
+}
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      console.log(email);
+      setIsTyping(false);
+    }, 500);
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  });
+
   return (
     <div className="App">
       <h1>Login</h1>
-      <input type={"text"}></input>
+      <h1>is typing: {isTyping ? "yes" : "no"}</h1>
+      <Input
+        placeholder="email"
+        style={{ width: 300 }}
+        value={email}
+        onChange={(e) => {
+          setIsTyping(true);
+          setEmail(e.target.value);
+        }}
+      ></Input>
     </div>
   );
 }
